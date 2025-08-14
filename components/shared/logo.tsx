f2eed5321115847837e463f72a9b7054a321b6ee
Shukuruloh <shukurxondev@gmail.com>
2025-08-14 11:58:59 +0500

    ozgarish

--- components/shared/logo.tsx
+++ components/shared/logo.tsx
@@ -5,13 +5,13 @@ function Logo() {
 	return (
 		<Link href={'/'} className='flex items-center gap-2'>
 			<Image
-				src={'/logo.png'}
+				src={'/logo.svg'}
 				alt='logo'
 				width={50}
 				height={50}
 				className='rounded-full'
 			/>
-			<h1 className=' font-roboto text-3xl font-bold '>Learnix </h1>
+			<h1 className=' font-roboto text-3xl font-bold '>Isofit </h1>
 		</Link>
 	)
 }
